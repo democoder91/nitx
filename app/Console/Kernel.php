d0d8967d4230da3a199f7ac9a9696631e0133eb7
mohamed abdullah <demo.composer2@gmail.com>
2023-02-11 17:18:08 +0200

    Added a method to the kernel to check for the squence end date

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,7 +3,9 @@
 namespace App\Console;
 
 use App\Events\ScreenAds;
-use App\Models\Screen;
+use App\Jobs\CheckForExpiredSequences;
+use App\Models\ScreenGroup;
+use App\Models\Sequence;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -20,6 +22,23 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('updateAds')->everyMinute();
         $schedule->command('updateSequenceStatus')->everyMinute();
+
+        // echo "Schedule is running" every minute
+        $schedule->call(function () {
+            // retreive all screens
+            $screenGroups = ScreenGroup::all();
+        foreach ($screenGroups as $screenGroup) {
+            $sequence = Sequence::find($screenGroup->sequence_id);
+            if ($sequence != null) {
+                if ($sequence->end_date < now()){
+                    // change screen group sequence id to the default sequence id
+                    $screenGroup->sequence_id = Sequence::where('name', 'Default Sequence')->first()->id;
+                    $screenGroup->update();
+                }
+            }
+            
+        }
+        })->everyMinute();
     }
 
     /**

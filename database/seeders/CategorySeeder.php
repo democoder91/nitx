7ba4c4e495cffc2bcf66f5d61f25f49fd82ecd18
democoder91 <68160481+democoder91@users.noreply.github.com>
2023-02-13 08:25:22 +0200

    made the seeders check if the seeded data already exists

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,100 +16,208 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert([
-            'category' => 'Coffee',
-            'image' => '',
-            'icon' => 'ri-cup-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if category dose not exist
+        if (Category::where('category', 'Coffee')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Coffee',
+                'image' => '',
+                'icon' => 'ri-cup-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
 
-        DB::table('categories')->insert([
-            'category' => 'Coffee Roasters',
-            'image' => '',
-            'icon' => 'ri-cup-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // DB::table('categories')->insert([
+        //     'category' => 'Coffee',
+        //     'image' => '',
+        //     'icon' => 'ri-cup-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
 
-        DB::table('categories')->insert([
-            'category' => 'Restaurants',
-            'image' => '',
-            'icon' => 'ri-restaurant-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if category dose not exist
+        if (Category::where('category', 'Coffee Roasters')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Coffee Roasters',
+                'image' => '',
+                'icon' => 'ri-cup-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // DB::table('categories')->insert([
+        //     'category' => 'Coffee Roasters',
+        //     'image' => '',
+        //     'icon' => 'ri-cup-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        
+        // check if category dose not exist
+        if (Category::where('category', 'Restaurants')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Restaurants',
+                'image' => '',
+                'icon' => 'ri-restaurant-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
 
-        DB::table('categories')->insert([
-            'category' => 'Games',
-            'image' => '',
-            'icon' => 'ri-gamepad-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // DB::table('categories')->insert([
+        //     'category' => 'Restaurants',
+        //     'image' => '',
+        //     'icon' => 'ri-restaurant-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
 
-        DB::table('categories')->insert([
-            'category' => 'Books',
-            'image' => '',
-            'icon' => 'ri-book-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if category dose not exist
+        if (Category::where('category', 'Games')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Games',
+                'image' => '',
+                'icon' => 'ri-gamepad-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
 
-        DB::table('categories')->insert([
-            'category' => 'Fashion',
-            'image' => '',
-            'icon' => 'ri-markup-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // DB::table('categories')->insert([
+        //     'category' => 'Games',
+        //     'image' => '',
+        //     'icon' => 'ri-gamepad-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
 
-        DB::table('categories')->insert([
-            'category' => 'Tools and Equipment',
-            'image' => '',
-            'icon' => 'ri-tools-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if category dose not exist
+        if (Category::where('category', 'Books')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Books',
+                'image' => '',
+                'icon' => 'ri-book-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // DB::table('categories')->insert([
+        //     'category' => 'Books',
+        //     'image' => '',
+        //     'icon' => 'ri-book-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
 
-        DB::table('categories')->insert([
-            'category' => 'Medical',
-            'image' => '',
-            'icon' => 'ri-medicine-bottle-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if category dose not exist
+        if (Category::where('category', 'Fashion')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Fashion',
+                'image' => '',
+                'icon' => 'ri-markup-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
 
-        DB::table('categories')->insert([
-            'category' => 'Sports',
-            'image' => '',
-            'icon' => 'ri-riding-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // DB::table('categories')->insert([
+        //     'category' => 'Fashion',
+        //     'image' => '',
+        //     'icon' => 'ri-markup-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
 
-        DB::table('categories')->insert([
-            'category' => 'Clothing',
-            'image' => '',
-            'icon' => 'ri-shirt-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if category dose not exist
+        if (Category::where('category', 'Tools and Equipment')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Tools and Equipment',
+                'image' => '',
+                'icon' => 'ri-tools-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
 
-        DB::table('categories')->insert([
-            'category' => 'Sports',
-            'image' => '',
-            'icon' => 'ri-plant-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // DB::table('categories')->insert([
+        //     'category' => 'Tools and Equipment',
+        //     'image' => '',
+        //     'icon' => 'ri-tools-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
 
-        DB::table('categories')->insert([
-            'category' => 'Sports',
-            'image' => '',
-            'icon' => 'ri-roadster-line',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if category dose not exist
+        if (Category::where('category', 'Medical')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Medical',
+                'image' => '',
+                'icon' => 'ri-medicine-bottle-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // DB::table('categories')->insert([
+        //     'category' => 'Medical',
+        //     'image' => '',
+        //     'icon' => 'ri-medicine-bottle-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // check if category dose not exist
+        if (Category::where('category', 'Sports')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Sports',
+                'image' => '',
+                'icon' => 'ri-riding-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // DB::table('categories')->insert([
+        //     'category' => 'Sports',
+        //     'image' => '',
+        //     'icon' => 'ri-riding-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // check if category dose not exist
+        if (Category::where('category', 'Clothing')->doesntExist()) {
+            DB::table('categories')->insert([
+                'category' => 'Clothing',
+                'image' => '',
+                'icon' => 'ri-shirt-line',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+
+        // DB::table('categories')->insert([
+        //     'category' => 'Clothing',
+        //     'image' => '',
+        //     'icon' => 'ri-shirt-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // check if category dose not exist
+        
+        // DB::table('categories')->insert([
+        //     'category' => 'Sports',
+        //     'image' => '',
+        //     'icon' => 'ri-plant-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+
+        // DB::table('categories')->insert([
+        //     'category' => 'Sports',
+        //     'image' => '',
+        //     'icon' => 'ri-roadster-line',
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
     }
 }
--- database/seeders/SequenceDay.php
+++ database/seeders/SequenceDay.php
@@ -10,46 +10,65 @@ class SequenceDay extends Seeder
 {
     public function run()
     {
-        DB::table('sequence_days')->insert([
-            'name' => 'Sunday',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if sequence_day dose not exist
+        if (DB::table('sequence_days')->where('name', 'Sunday')->doesntExist()) {
+            DB::table('sequence_days')->insert([
+                'name' => 'Sunday',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // check if sequence_day dose not exist
+        if (DB::table('sequence_days')->where('name', 'Monday')->doesntExist()) {
+            DB::table('sequence_days')->insert([
+                'name' => 'Monday',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
 
-        DB::table('sequence_days')->insert([
-            'name' => 'Monday',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // check if sequence_day dose not exist
+        if (DB::table('sequence_days')->where('name', 'Tuesday')->doesntExist()) {
+            DB::table('sequence_days')->insert([
+                'name' => 'Tuesday',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // check if sequence_day dose not exist
+        if (DB::table('sequence_days')->where('name', 'Wednesday')->doesntExist()) {
+            DB::table('sequence_days')->insert([
+                'name' => 'Wednesday',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // check if sequence_day dose not exist
+        if (DB::table('sequence_days')->where('name', 'Thursday')->doesntExist()) {
+            DB::table('sequence_days')->insert([
+                'name' => 'Thursday',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // check if sequence_day dose not exist
+        if (DB::table('sequence_days')->where('name', 'Friday')->doesntExist()) {
+            DB::table('sequence_days')->insert([
+                'name' => 'Friday',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+        // check if sequence_day dose not exist
+        if (DB::table('sequence_days')->where('name', 'Saturday')->doesntExist()) {
+            DB::table('sequence_days')->insert([
+                'name' => 'Saturday',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
 
-        DB::table('sequence_days')->insert([
-            'name' => 'Tuesday',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
 
-        DB::table('sequence_days')->insert([
-            'name' => 'Wednesday',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('sequence_days')->insert([
-            'name' => 'Thursday',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('sequence_days')->insert([
-            'name' => 'Friday',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('sequence_days')->insert([
-            'name' => 'Saturday',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        
     }
 }

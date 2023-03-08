a80e1ff8eec4d3680ad4654f813f58feb49e8490
democoder91 <68160481+democoder91@users.noreply.github.com>
2023-03-08 22:46:33 +0200

    fixed the live sequence status after update

--- app/Services/SequenceService.php
+++ app/Services/SequenceService.php
@@ -15,7 +15,13 @@ class SequenceService
     {
         // if the sequence start date is before now and the end date is after now or the end date is null then the sequence is ready
         if (( $sequence->start_date < now() && $sequence->end_date >= now()) || ($sequence->start_date < now() && $sequence->end_date == null)){
+            // chek if the sequence is assigned to any screen group
+            $screenGroups = ScreenGroup::where('sequence_id', $sequence->id)->get();
+            if (count($screenGroups) > 0) {
+                $sequence->status = Status::Live->value;
+            } else {
             $sequence->status = Status::Ready->value;
+            }
         } else if ($sequence->end_date != Null && $sequence->end_date < now()) {
 
             $sequence->status = Status::Ended->value;
